0ebbf882a46c0d12ef49d35b797e02a804542e82
张新望 <zhangxinwang@zhangxinwangdeMacBook-Pro.local>
2016-11-23 13:58:31 +0800

    add vue-router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,32 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App'
+import goods from 'components/goods/goods.vue'
+import ratings from 'components/ratings/ratings.vue'
+import sellers from 'components/seller/seller.vue'
 
+Vue.use(VueRouter)
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
+
+let routes = [
+  {
+    path: '/goods', component: goods
+  },
+  {
+    path: '/ratings', component: ratings
+  },
+  {
+    path: '/sellers', component: sellers
+  }
+]
+const router = new VueRouter({
+  routes,
+  linkActiveClass: 'active'
 })
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
+
+router.push('/goods')
