70c0803bfc3ffa5f081c21cd2f934bbe9ea5ad66
张新望 <zhangxinwang@ZhangxwdeMacBook-Pro.local>
2016-12-23 01:22:24 +0800

    小球动画

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -18,14 +18,11 @@
       </div>
     </div>
     <div class="ball-container">
-      <transition-group name="drop"
-                        v-on:before-enter="beforeEnter"
-                        v-on:enter="enter"
-                        v-on:after-enter="afterEnter">
-        <div class="ball" v-for="ball in balls" :key="ball" v-show="ball.show">
+      <transition name="drop"  v-for="ball in balls" @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter">
+        <div class="ball" :key="ball" v-show="ball.show">
           <div class="inner inner-hook"></div>
         </div>
-      </transition-group>
+      </transition>
     </div>
   </div>
 </template>
@@ -121,7 +118,6 @@
         }
       },
       beforeEnter(el) {
-        console.log(el)
         let count = this.balls.length
         while (count--) {
           let ball = this.balls[count]
@@ -135,10 +131,11 @@
             let inner = el.getElementsByClassName('inner-hook')[0]
             inner.style.webkitTransform = `translate3d(${x}px,0,0)`
             inner.style.transform = `translate3d(${x}px,0,0)`
+            console.log(this.balls)
           }
         }
       },
-      enter(el) {
+      enter(el, done) {
         /* eslint-disable no-unused-vars */
         let rf = el.offsetHeight
         this.$nextTick(() => {
@@ -148,6 +145,7 @@
           inner.style.webkitTransform = 'translate3d(0,0,0)'
           inner.style.transform = 'translate3d(0,0,0)'
         })
+        done()
       },
       afterEnter(el) {
         let ball = this.dropBall.shift()
@@ -253,11 +251,11 @@
         bottom 22px
         z-index 200
         &.drop-enter-active, &.drop-leave-active
-          transition all 0.4
+          transition all 0.5s cubic-bezier(.49,-0.29,.75,.41)
           .inner
             width 16px
             height 16px
             border-radius 50%
             background rgb(0,160,220)
-            transition all 0.4
+            transition all 0.5s linear
 </style>
