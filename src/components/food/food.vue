a327703fe91bb19fc1e9ae90e42e3aff955ebda3
Zegendary <zxw1992513@qq.com>
2017-01-16 23:53:15 +0800

    更新仓库

---
+++ src/components/food/food.vue
@@ -0,0 +1,185 @@
+<template>
+  <transition name="move">
+    <div class="food" v-show="foodFlag" ref="food">
+      <div class="food-content">
+        <div class="image-header">
+          <img :src="food.image">
+          <div class="back" @click="hide">
+            <i class="icon-arrow_lift"></i>
+          </div>
+        </div>
+        <div class="content">
+          <h1 class="title">{{ food.name }}</h1>
+          <div class="detail">
+            <span class="sell-count">月售{{ food.sellCount }}份</span>
+            <span class="rating">好评率{{ food.rating }}%</span>
+          </div>
+          <div class="price">
+            <span class="now-price">￥{{ food.price }}</span>
+            <span class="old-price" v-show="food.oldPrice">￥{{ food.oldPrice }}</span>
+          </div>
+          <div class="cartcontrol-wrapper">
+            <cartcontrol :food="food"></cartcontrol>
+          </div>
+          <transition name="fade">
+            <div @click.stop.prevent="addFirst($event)" class="buy" v-show="!food.count || food.count === 0">加入购物车</div>
+          </transition>
+        </div>
+        <split v-show="food.info"></split>
+        <div class="info" v-show="food.info">
+          <h1 class="title">商品信息</h1>
+          <p class="text">{{food.info}}</p>
+        </div>
+        <split></split>
+        <div class="rating">
+          <h1 class="title">商品评价</h1>
+
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+<script type="text/ecmascript-6">
+  import BSroll from 'better-scroll'
+  import Vue from 'vue'
+  import cartcontrol from 'components/cartcontrol/cartcontrol'
+  import split from 'components/split/split'
+  import Bus from '../../bus.js'
+
+  export default{
+    props: {
+      food: {
+        type: Object
+      }
+    },
+    data() {
+      return {
+        foodFlag: false
+      }
+    },
+    methods: {
+      show() {
+        this.foodFlag = true
+        this.$nextTick(() => {
+          if (!this.scroll) {
+            this.scroll = new BSroll(this.$refs.food,{
+              click: true
+            })
+          }
+        })
+      },
+      hide() {
+        this.foodFlag = false
+      },
+      addFirst(event) {
+        if (!event._constructed) {
+          return
+        }
+        Vue.set(this.food,'count',1)
+        Bus.$emit('cart.add',event.target)
+      }
+    },
+    components: {
+      cartcontrol,
+      split
+    }
+  }
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+  .food
+    position fixed
+    top 0
+    left 0
+    right 0
+    bottom 40px
+    z-index 30
+    width 100%
+    background #fff
+    transition all 0.2s linear
+    transform translate3d(0,0,0)
+    &.move-enter,&.move-leave-active
+      transform translate3d(100%,0,0)
+    .image-header
+      position relative
+      width 100%
+      height 0
+      padding-top 100%
+      img
+        position absolute
+        top 0
+        left 0
+        height 100%
+        width 100%
+      .back
+        position absolute
+        top 10px
+        left 0
+        .icon-arrow_lift
+          display block
+          padding 10px
+          font-size 20px
+          color #fff
+    .content
+      position relative
+      padding 18px
+      .title
+        line-height 14px
+        margin-bottom 8px
+        font-size 14px
+        font-weight 700
+        color rgb(7,17,27)
+      .detail
+        margin-bottom 18px
+        line-height 10px
+        height 10px
+        font-size 0
+        .sell-count,.rating
+          font-size 10px
+          color rgb(147,153,159)
+        .sell-count
+          margin-right 12px
+      .price
+        font-weight 700
+        line-height 24px
+        .now-price
+          margin-right 8px
+          font-size 14px
+          color rgb(240,20,20 )
+        .old-price
+          text-decoration line-through
+          font-size 10px
+          color rgb(147,153,159 )
+      .cartcontrol-wrapper
+        position absolute
+        right 12px
+        bottom 12px
+      .buy
+        position absolute
+        right 18px
+        bottom 18px
+        z-index 10
+        height 24px
+        line-height 24px
+        padding 0 12px
+        box-sizing border-box
+        border-radius 12px
+        font-size 10px
+        color #fff
+        background rgb(0,160,220)
+        transition all 0.2 linear
+        opacity 1
+        &.fade-enter,&.fade-leave
+          opacity 0
+    .info
+      padding 18px
+      .title
+        font-size 14px
+        margin-bottom 6px
+        color rgb(7,17,27)
+      .text
+        line-height 24px
+        padding 0 8px
+        font-size 12px
+        font-weight 200
+        color rgb(77,85,93)
+</style>
--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -14,7 +14,7 @@
         <li v-for="item in goods" class="food-list food-list-hook">
           <h1 class="title border-1px">{{ item.name }}</h1>
           <ul>
-            <li v-for="food in item.foods" class="food-item">
+            <li @click="selectFood(food,$event)" v-for="food in item.foods" class="food-item">
               <div class="icon">
                 <img :src="food.icon" width="57px" height="57px">
               </div>
@@ -25,7 +25,7 @@
                   <span class="count">月售{{ food.sellCount }}份</span><span>好评率{{ food.rating }}</span>
                 </div>
                 <div class="price">
-                  <span class="now">￥{{ food.price }}</span>
+                  <span class="now-price">￥{{ food.price }}</span>
                   <span class="old-price" v-show="food.oldPrice">￥{{ food.oldPrice }}</span>
                 </div>
                 <div class="cartcontrol-wrapper">
@@ -38,12 +38,14 @@
       </ul>
     </div>
     <shopcart :select-foods="selectFoods" :delivery-price='seller.deliveryPrice' :min-price='seller.minPrice'></shopcart>
+    <food :food="selectedFood" ref="food"></food>
   </div>
 </template>
 <script type="text/ecmascript-6">
   import BSroll from 'better-scroll'
   import shopcart from 'components/shopcart/shopcart'
   import cartcontrol from 'components/cartcontrol/cartcontrol'
+  import food from 'components/food/food'
 
   const ERR_OK = 0
 
@@ -52,7 +54,8 @@
       return {
         goods: [],
         listHeight: [],
-        scrollY: 0
+        scrollY: 0,
+        selectedFood: { }
       }
     },
     created() {
@@ -130,11 +133,19 @@
         let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook')
         let element = foodList[index]
         this.foodsScroll.scrollToElement(element, 300)
+      },
+      selectFood(food,event) {
+        if (!event._constructed) {
+          return
+        }
+        this.selectedFood = food
+        this.$refs.food.show()
       }
     },
     components: {
       shopcart,
-      cartcontrol
+      cartcontrol,
+      food
     }
   }
 </script>
@@ -232,11 +243,11 @@
           .price
             font-weight 700
             line-height 24px
-            .now
+            .now-price
               margin-right 8px
               font-size 14px
               color rgb(240,20,20 )
-            .old
+            .old-price
               text-decoration line-through
               font-size 10px
               color rgb(147,153,159 )
